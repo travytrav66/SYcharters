5d15305442e220f3749b2295a4b6306003a025c1
Travis <tcarr8966@gmail.com>
2026-04-30 10:14:40 -0700

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/charter/',
+  base: '/SYcharters/',
   build: {
     rollupOptions: {
       input: {
